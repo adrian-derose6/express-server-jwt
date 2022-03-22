c45e8b6a7b292181d6e24909810a70822f9a3077
Adrian DeRose <adrian.derose6@gmail.com>
2022-03-22 12:56:38 -0500

    Create JWT when login Controller receives login info

--- controllers/main.js
+++ controllers/main.js
@@ -1,15 +1,38 @@
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error');
+
 const login = async (req, res) => {
+	const { username, password } = req.body;
+	console.log(username, password);
+
+	// a) Mongoose validation
+	// b) Joi
+	// c) check in the controller
+
+	if (!username || !password) {
+		throw new CustomAPIError('Please provide email and password', 400);
+	}
+
+	// Normally id provided by DB
+	// JWT_SECRET: Always use long, complex and unguessable string value in production!
+	const id = new Date().getDate();
+
+	// try to keep payload small, better experience for user
+	const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+		expiresIn: '30d',
+	});
+
+	res.status(200).json({ msg: 'user created', token });
+
 	res.send('Mock Login/Register/Signup Route');
 };
 
 const dashboard = async (req, res) => {
 	const randomNumber = Math.floor(Math.random() * 100);
-	res
-		.status(200)
-		.json({
-			msg: 'Hello',
-			secret: `Here is your authorized data, your random number is ${randomNumber}`,
-		});
+	res.status(200).json({
+		msg: 'Hello',
+		secret: `Here is your authorized data, your random number is ${randomNumber}`,
+	});
 };
 
 module.exports = {
