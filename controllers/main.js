49b97266d77bee25f1da03677276bab3ef493e7b
Adrian DeRose <adrian.derose6@gmail.com>
2022-03-24 10:58:25 -0500

    Move Auth token authorization logic to authMiddleware

--- controllers/main.js
+++ controllers/main.js
@@ -28,24 +28,13 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-	const authHeader = req.headers.authorization;
-	if (!authHeader || !authHeader.startsWith('Bearer ')) {
-		throw new CustomAPIError('No token provided', 401);
-	}
+	console.log(req.user);
+	const randomNumber = Math.floor(Math.random() * 100);
 
-	const token = authHeader.split(' ')[1];
-
-	try {
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
-		const randomNumber = Math.floor(Math.random() * 100);
-
-		res.status(200).json({
-			msg: `Hello ${decoded.username}`,
-			secret: `Here is your authorized data, your random number is ${randomNumber}`,
-		});
-	} catch (error) {
-		throw new CustomAPIError('Not authorized to access this route', 401);
-	}
+	res.status(200).json({
+		msg: `Hello ${req.user.username}`,
+		secret: `Here is your authorized data, your random number is ${randomNumber}`,
+	});
 };
 
 module.exports = {
